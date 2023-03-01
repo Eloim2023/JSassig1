12eb1db73710952cfd5202bc34b6d3b759deabb8
Eloim2023 <124946573+Eloim2023@users.noreply.github.com>
2023-02-28 18:45:51 -0800

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,28 @@
+console.log("Script")
+
+let webName ="YouMusic";
+let webGreetings = "Welcome to the best Music videos Web Page";
+let webLogIn; 
+
+let thumbnails = "img/music.png";
+
+let webSelect = "Please check the box of your music gender";
+let webgenderCountry = "You like Country?";
+let webgenderPop = "You like Pop?";
+let webgenderRock = "You like Rock?";
+let webgenderMetal = "You like Metal?";
+let webgenderSoftRock = "You like Soft Rock?";
+let webgenderSalsa = "You like Salsa?";
+
+
+document.write("<h1>" + webName + "</h1>");
+document.write("<h3>" + webGreetings + "</h3>");
+document.write("<img src='" + thumbnails + "' alt='Music'>");
+document.write("<h3>" + webSelect + "</h3>");
+document.write("<h4>" + webgenderCountry + "</h4>");
+document.write("<h4>" + webgenderPop + "</h4>");
+document.write("<h4>" + webgenderRock + "</h4>")
+document.write("<h4>" + webgenderMetal + "</h4>")
+document.write("<h4>" + webgenderSoftRock + "</h4>")
+document.write("<h4>" + webgenderSalsa + "</h4>")
+
